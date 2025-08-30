5112c3151e49e9dfb29dac5be83ffb62149b180f
SrCarlito <carlitisamuel@gmail.com>
2025-08-30 12:59:56 -0400

    xd

--- src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as HomePage } from "./HomePage";
-export { default as PackageDetailPage } from "./PackageDetailPage";
-export { default as ReservationFormPage } from "./ReservationFormPage";
-export { default as AdminDashboardPage } from "./AdminDashboardPage";
